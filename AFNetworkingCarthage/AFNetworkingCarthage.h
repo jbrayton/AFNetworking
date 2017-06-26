28fa34be43004662b01c63090590d8efe562224a
John Brayton <jbrayton@goldenhillsoftware.com>
2017-06-26 17:24:32 -0400

    Added Carthage-compatible project based on 1.3.4 tag.

---
+++ AFNetworkingCarthage/AFNetworkingCarthage.h
@@ -0,0 +1,19 @@
+//
+//  AFNetworkingCarthage.h
+//  AFNetworkingCarthage
+//
+//  Created by John Brayton on 6/26/17.
+//  Copyright © 2017 John Brayton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFNetworkingCarthage.
+FOUNDATION_EXPORT double AFNetworkingCarthageVersionNumber;
+
+//! Project version string for AFNetworkingCarthage.
+FOUNDATION_EXPORT const unsigned char AFNetworkingCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFNetworkingCarthage/PublicHeader.h>
+
+
